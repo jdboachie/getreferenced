d5606f3f61066f9d5988bc922b4977638b03d9a5
Jude Boachie <jdboachie@gmail.com>
2025-05-13 20:58:10 +0000

    hope this fixes it

---
+++ app/layout.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from "next";
+
+import LocalFont from "next/font/local";
+import { Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+
+const sans = LocalFont({
+  variable: "--font-sans",
+  src: [
+    {
+      path: 'fonts/bold.woff2',
+      weight: '700',
+    },
+    {
+      path: 'fonts/medium.woff2',
+      weight: '500',
+    },
+    {
+      path: 'fonts/regular.woff2',
+      weight: '400',
+    },
+  ]
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Approved 🤌",
+  description: "Recommendme - by us at fastfast.computer",
+  icons: {
+    icon: "/convex.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ConvexAuthNextjsServerProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${sans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ConvexClientProvider>
+              {children}
+            </ConvexClientProvider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ConvexAuthNextjsServerProvider>
+  );
+}
---
+++ components/nav/breadcrumbs.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { ChevronRightIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+const capitalize = (s: string) =>
+  s
+    .replace(/-/g, ' ')
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+
+const Breadcrumbs = () => {
+
+  const pathname = usePathname();
+  const segments = pathname.split('/').filter(Boolean);
+
+  return (
+    <Breadcrumb className="mb-8 mt-4">
+      <BreadcrumbList>
+        {/* <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link className="text-2xl sm:text-2xl font-bold" href="/">Overview</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem> */}
+
+        {segments.length === 1 ? (
+          <BreadcrumbPage className="text-2xl sm:text-3xl font-medium">Overview</BreadcrumbPage>
+        ) :
+          <>
+            {segments.slice(1).map((segment, index) => {
+              const href = '/' + segments.slice(0, index + 2).join('/');
+              const isFirst = index === 0;
+              const isLast = index === segments.length - 2;
+
+              return (
+                <div key={href} className="flex items-center gap-1">
+                  {!isFirst &&
+                    <BreadcrumbSeparator className="[&>svg]:size-6">
+                      <ChevronRightIcon className="stroke-[1.5]" />
+                    </BreadcrumbSeparator>
+                  }
+                  <BreadcrumbItem>
+                    {isLast ? (
+                      <BreadcrumbPage className="text-2xl sm:text-3xl font-medium">{capitalize(segment)}</BreadcrumbPage>
+                    ) : (
+                      <BreadcrumbLink asChild>
+                        <Link className="text-2xl sm:text-3xl font-medium" href={href}>{capitalize(segment)}</Link>
+                      </BreadcrumbLink>
+                    )}
+                  </BreadcrumbItem>
+                </div>
+              );
+            })}
+          </>
+        }
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default Breadcrumbs;
