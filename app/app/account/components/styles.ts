f9348cb6df8c05deaed379c1ebee5c4f66bbd9e3
Jude Boachie <jdboachie@gmail.com>
2025-06-12 12:42:15 +0000

    Update styles.ts

--- app/app/account/components/styles.ts
+++ app/app/account/components/styles.ts
@@ -1,5 +1,5 @@
 export const profileCardStyles = {
-  card: 'border bg-primary-foreground dark:bg-background rounded-lg p-1',
-  cardContent: 'bg-background dark:bg-primary-foreground rounded-md shadow-xs border p-4 gap-4 flex flex-col',
+  card: 'border bg-primary-foreground dark:bg-background rounded-xl p-1',
+  cardContent: 'bg-background dark:bg-primary-foreground rounded-lg shadow-xs border p-4 gap-4 flex flex-col',
   cardFooter: 'md:gap-2 gap-4 flex max-sm:flex-col sm:justify-between items-center rounded-b-lg p-3'
 }
